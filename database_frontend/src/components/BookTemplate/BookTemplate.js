3df79ed7e879541925228e02999e3321005cac79
Konstantinos Perifanos <kperifanos2@gmail.com>
2023-05-13 19:06:59 +0300

    Bug wrong book view fixed

--- database_frontend/src/components/BookTemplate/BookTemplate.js
+++ database_frontend/src/components/BookTemplate/BookTemplate.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './BookTemplate.css';
 import { NavLink } from 'react-router-dom';
 
-const BookTemplate = ({onBookClicked2,index,title,cover}) => {
+const BookTemplate = ({onBookClicked2,index,title,cover,isbn}) => {
 
 
     return(
         <div>
-            <NavLink onClick={() => onBookClicked2(index)} to='/book'><img alt='' className='book_cover' src={`${cover}`} width='260px' height='420px'/></NavLink>
+            <NavLink onClick={() => onBookClicked2(isbn)} to='/book'><img alt='' className='book_cover' src={`${cover}`} width='260px' height='420px'/></NavLink>
             <span className='book_title'>{`${title}`}</span>
         </div>
     );
